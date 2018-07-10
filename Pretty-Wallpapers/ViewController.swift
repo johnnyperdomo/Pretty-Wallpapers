dd1cdbec7086eb57d694c8e9d83aa9f940ceb162
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-07-09 21:22:01 -0400

    create UI

--- Pretty-Wallpapers/ViewController.swift
+++ Pretty-Wallpapers/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func exportBtnPressed(_ sender: Any) {
+        
     }
-
+    
+    @IBAction func randomBtnPressed(_ sender: Any) {
+        
+    }
+    
 
 }
 
