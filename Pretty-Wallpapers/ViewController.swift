1bec4758cd5f84bc8d5905f513aa07a280dc149c
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-07-10 21:28:25 -0400

    add animations / swipe to get images

--- Pretty-Wallpapers/ViewController.swift
+++ Pretty-Wallpapers/ViewController.swift
@@ -9,16 +9,19 @@
 import UIKit
 import Spring
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var imageText: UILabel!
+    @IBOutlet weak var imageView: SpringImageView!
+    @IBOutlet weak var imageText: SpringLabel!
+    @IBOutlet weak var exportBtn: SpringButton!
     
     let urlString = "https://source.unsplash.com/collection/1065412"
     
+
         override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addSwipe()
+        imageView.autostart = true
     }
 
     @IBAction func exportBtnPressed(_ sender: Any) {
@@ -29,12 +32,54 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func randomBtnPressed(_ sender: Any) {
+    func addSwipe() {
+        let swipeleft = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft))
+        swipeleft.direction = UISwipeGestureRecognizerDirection.left
         
-        getImage()
-        imageText.isHidden = true
+        let swiperight = UISwipeGestureRecognizer(target: self, action: #selector(swipeRight))
+        swiperight.direction = UISwipeGestureRecognizerDirection.right
+        
+        imageView.addGestureRecognizer(swiperight)
+        imageView.addGestureRecognizer(swipeleft)
     }
     
+    @objc func swipeLeft() {
+        
+        imageText.animation = "fadeOut"
+        imageText.duration = 1.0
+        imageText.animate()
+        imageText.isHidden = true
+        
+        imageView.velocity = 10
+        imageView.animation = "fadeInLeft"
+        imageView.curve = "easeIn"
+        imageView.animate()
+        getImage()
+        
+        exportBtn.animation = "fadeIn"
+        exportBtn.delay = 0.5
+        exportBtn.duration = 1.5
+        exportBtn.animate()
+    }
+    
+    @objc func swipeRight() {
+        
+        imageText.animation = "fadeOut"
+        imageText.duration = 1.0
+        imageText.animate()
+        imageText.isHidden = true
+        
+        imageView.velocity = 10
+        imageView.animation = "fadeInRight"
+        imageView.curve = "easeIn"
+        imageView.animate()
+        getImage()
+        
+        exportBtn.animation = "fadeIn"
+        exportBtn.delay = 0.5
+        exportBtn.duration = 1.5
+        exportBtn.animate()
+    }
 
     func getImage() {
         
